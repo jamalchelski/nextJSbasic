d815c4f45a870bb9b28adad5ad98f788889f5940
jamalChelski <ifista202522035@gmail.com>
2022-01-16 23:41:08 +0700

    get static paths

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,25 +1,25 @@
 import Layout from "../../components/Layout";
-
+import { useRouter } from "next/router";
+import styles from "../../styles/Users.module.css";
 interface UserProps {
     dataUsers: Array<any>;
 }
 
-export default function User(props: dataUsers) {
+export default function User(props: UserProps) {
     const { dataUsers } = props;
-
+    const router = useRouter();
     console.log(dataUsers)
     return (
+
         <Layout pageTitle="Users Page">
-            {dataUsers.map(user => {
-                return (
-                    <>
-                        <p>{user.name}</p>
-                        <p>{user.username}</p>
-                    </>
-                )
-            })}
+            {dataUsers.map((user) => (
+                <div key={user.id} onClick={() => router.push(`/users/${user.id}`)} className={styles.card}>
+                    <p>{user.name}</p>
+                    <p>{user.email}</p>
+                </div>
+            ))}
         </Layout>
-    )
+    );
 }
 
 export async function getStaticProps() {
